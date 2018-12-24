c1ae36e54214d78720b8f10240daa764d818a53c
ColinLiu <ColinLiu@boyaa.com>
2018-12-24 18:10:18 +0800

    增加图片web server

--- server.go
+++ server.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+)
 
 func main() {
 	fmt.Println("web server start...")
+
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./images"))))
+	go http.ListenAndServe(":8090", nil)
+
+	sigc := make(chan os.Signal)
+	signal.Notify(sigc, os.Interrupt)
+	select {
+	case <-sigc:
+	}
 }

00ec8a07e7694ee6db516d722174c3ccf256a68f
ColinLiu <colinlets@gmail.com>
2018-12-24 22:03:51 +0800

    同步进度

--- server.go
+++ server.go
@@ -12,12 +12,12 @@ func main() {
 	fmt.Println("web server start...")
 
 	//web
-	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
+	http.Handle("/wechat/", http.StripPrefix("/wechat/", http.FileServer(http.Dir("./images"))))
 
 	//router
 	http.HandleFunc("/api/test", controller.Api.Test)
 
-	go http.ListenAndServe(":8090", nil)
+	go http.ListenAndServe(":80", nil)
 
 	sigc := make(chan os.Signal)
 	signal.Notify(sigc, os.Interrupt)

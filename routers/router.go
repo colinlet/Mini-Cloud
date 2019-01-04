dc5fdd8dbe958b78dd1cef29b90b709b46005dba
colinlet <Colinlets@gmail.com>
2019-01-04 16:26:00 +0800

    移除冗余代码

--- routers/router.go
+++ routers/router.go
@@ -33,17 +33,6 @@ func InitRouter() *gin.Engine {
 		//编辑地址
 
 		//获取订单
-
-		apiv1.GET("/test", v1.GetTest)
-
-		//获取标签列表
-		apiv1.GET("/tags", v1.GetTags)
-		//新建标签
-		apiv1.POST("/tags", v1.AddTag)
-		//更新指定标签
-		apiv1.PUT("/tags/:id", v1.EditTag)
-		//删除指定标签
-		apiv1.DELETE("/tags/:id", v1.DeleteTag)
 	}
 
 	//图片资源
